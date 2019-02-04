6bebdbebe08285ed9a52b09139e2cbc0a7abf4a0
Albermonte <14013679+Albermonte@users.noreply.github.com>
2019-02-04 20:18:35 +0100

    Scroll bar on firefox not working

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <Header />
   <div class="flex-container">
     <Sidebar />
-    <div class="content-container nq-style scrollbar-themed">
+    <div class="content-container nq-style scrollbar-themed"> <!-- Scrollbar not working on firefox -->
       <router-view></router-view>
     </div>
   </div>
