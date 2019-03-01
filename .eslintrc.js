1b35918daed7bddfa7d3eeb30fbc1a9f87d3e84c
brantje <brantje@gmail.com>
2019-03-01 21:41:14 +0100

    initial config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    "root": true,
+    "env": {
+        "node": true
+    },
+    "extends": [],
+    "rules": {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "quotemark": [true, "single"],
+        "indent": ['warn', 4],
+        "interface-name": false,
+        "ordered-imports": false,
+        "object-literal-sort-keys": false,
+        "max-classes-per-file": ['warn', 3],
+        "no-consecutive-blank-lines": [false],
+        "curly": ['error', "multi-line"],
+        "variable-name": [true, "ban-keywords", "check-format", "allow-leading-underscore"],
+        "member-ordering": false
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true,
+            "experimentalObjectRestSpread": true
+        },
+        "parser": "babel-eslint",
+
+    },
 };
