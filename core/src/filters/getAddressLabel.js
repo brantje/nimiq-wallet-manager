3cd5725fbd740f94a52979cf119afc0322a6b1c0
brantje <brantje@gmail.com>
2019-03-09 10:31:06 +0100

    Implement Nimiq AddressBook

--- core/src/filters/getAddressLabel.js
+++ core/src/filters/getAddressLabel.js
@@ -1,6 +1,10 @@
 import store from 'store'
-
+import {AddressBook} from 'utils/AddressBook'
 export function getAddressLabel(address) {
+    let nimiqAddressBook = AddressBook.getLabel(address)
+    if(nimiqAddressBook){
+        return nimiqAddressBook
+    }
     let book = [...store.state.contact.contacts, ...store.state.wallet.wallets]
     for (let i of book) {
         if (i.address === address) {
