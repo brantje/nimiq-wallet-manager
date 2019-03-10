fe9a032c0e11f14d8e6b69e370b574afebf9feeb
Sander <brantje@gmail.com>
2019-03-10 14:19:02 +0000

    Disable chain to db sync. Fixes #105 (#109)

--- services/NimiqService.js
+++ services/NimiqService.js
@@ -14,15 +14,16 @@ class NimiqService {
 
     async headChanged(head) {
         let block = await this.helper.getBlockByHash(head.hash().toHex())
-        let transactions = []
+        // let transactions = []
         for (let tx of block.transactions) {
             let newTx = Object.assign(new Transaction(), await this.helper.getTransactionByHash(tx))
-            try{
+            /*try{
                 await newTx.save()
             } catch (e){
                 Nimiq.Log.w(NimiqService, 'Error while inserting tx')
             }
             transactions.push(newTx._id)
+            */
             try {
                 Nimiq.Log.i(NimiqService, 'Clearing caches for:', newTx.to, newTx.from)
                 Cache.invalidate(newTx.to, newTx.from)
@@ -31,13 +32,13 @@ class NimiqService {
             }
         }
 
-        let newBlock = Object.assign(new Block(), block)
+        /*let newBlock = Object.assign(new Block(), block)
         newBlock.transactions = transactions
         try {
             newBlock.save()
         } catch (e) {
             Nimiq.Log.w(NimiqService, 'Error while inserting block')
-        }
+        }*/
     }
 
     async blockReverted(head) {
