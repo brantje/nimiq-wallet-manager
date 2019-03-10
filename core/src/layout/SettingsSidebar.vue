aee3caa2d570fa89a6e8c60fbdb84920db403d07
Sander <brantje@gmail.com>
2019-03-10 14:53:07 +0000

    Style settings menu (#110)

--- core/src/layout/SettingsSidebar.vue
+++ core/src/layout/SettingsSidebar.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="sidenav scrollbar-themed">
         <div class="nq-card">
-            <div class="nq-card-body">
-                Personal settings
+            <div class="nq-card-header">
+                <h1 class="nq-h1">
+                    Personal settings
+                </h1>
+            </div>
+            <div class="menu nq-style">
                 <ul>
                     <li>
                         <RouterLink to="/settings/user/account">
@@ -20,12 +24,6 @@
                         </RouterLink>
                     </li>
                 </ul>
-
-                <!--Admin settings-->
-                <!--<ul>-->
-                <!--<li>Users</li>-->
-                <!--<li>Nimiq</li>-->
-                <!--</ul>-->
             </div>
         </div>
     </div>
@@ -39,12 +37,8 @@ import {lunaToCoins} from 'filters/lunaToCoins'
 import Identicon from 'components/Identicon.vue'
 
 export default {
-    filters: {
-
-    },
-    components: {
-
-    },
+    filters: {},
+    components: {},
     data() {
         return {}
     },
@@ -52,15 +46,41 @@ export default {
     created() {
 
     },
-    methods: {
-
-    }
+    methods: {}
 }
 </script>
 
-<style scoped>
-    .nq-card{
+<style scoped lang="scss">
+    .nq-card {
         animation: slideInDown 0.5s;
         cursor: pointer;
+    }
+    .nq-card-header{
+        text-align: center;
+        background-color: rgba(31,35,72,.07);
+    }
+    .menu{
+        ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            border-left: 1px solid rgba(31,35,72,.07);
+            li {
+                margin: 0;
+                a {
+                    &.router-link-active{
+                        background-color: #fff;
+                        color: #24292e;
+                        cursor: default;
+                        font-weight: 600;
+                        border-left: 2px solid var(--nimiq-blue);
+                        padding: 8px 8px;
+                    }
+                    display: block;
+                    padding: 8px 10px;
+                    border-bottom: 1px solid #e1e4e8;
+                }
+            }
+        }
     }
 </style>
